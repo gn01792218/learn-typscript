95c57e0446ae552508c2728e9530b51ecee4616d
MSI\gn01792218 <gn01792218@hotmail.com>
2022-07-03 14:18:56 +0800

    新增literal type 動態使用的方式

--- src/Basic/literalType.ts
+++ src/Basic/literalType.ts
@@ -22,4 +22,25 @@ function calculate(num1:number,num2:number,methods:'+'|'-'|'*'|'/'):number{
     }
 }
 console.log(calculate(1,2,'*')) 
-console.log(calculate(1,2,'乘'))   //error TS2345: Argument of type '"乘"' is not assignable to parameter of type '"+" | "-" | "*" | "/"'.
+// console.log(calculate(1,2,'乘'))   //error TS2345: Argument of type '"乘"' is not assignable to parameter of type '"+" | "-" | "*" | "/"'.
+
+
+//進階使用 : 動態 literal types
+//1.基本使用
+type World = "word"
+type Greeting = `hello ${World}`
+
+//2.和union type 一起使用
+type EmailLocaleIDs = "welcome_email" | "email_heading";
+type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+ 
+type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`; //type AllLocaleIDs = "welcome_email_id" | "email_heading_id" | "footer_title_id" | "footer_sendoff_id"
+
+//3.承上，多個union type一起使用
+type Lang = "en" | "ja" | "pt";
+ 
+type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
+// type LocaleMessageIDs = "en_welcome_email_id" | "en_email_heading_id" | "en_footer_title_id" | "en_footer_sendoff_id" | "ja_welcome_email_id" | "ja_email_heading_id" | "ja_footer_title_id" | "ja_footer_sendoff_id" | "pt_welcome_email_id" | "pt_email_heading_id" | "pt_footer_title_id" | "pt_footer_sendoff_id"
+
+//詳細請參考官網
+// https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html
